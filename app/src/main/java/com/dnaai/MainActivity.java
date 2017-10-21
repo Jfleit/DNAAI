3188e63224c05fb52362af6324596e85d34bff69
brkozik <briankozik@gmail.com>
2017-10-21 11:59:40 -0700

    splash screen works

--- app/src/main/java/com/dnaai/MainActivity.java
+++ app/src/main/java/com/dnaai/MainActivity.java
@@ -2,13 +2,8 @@ package com.dnaai;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -101,11 +97,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.status) {
-            // Handle the camera action}
+
         }
 
         else if (id == R.id.configuration){
-
+            Intent intent = new Intent(this, Configuration.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
